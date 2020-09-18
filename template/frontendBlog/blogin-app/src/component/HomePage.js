1ad5e27825c95e7c1daee3ba77a6c74d69742b9a
BaldeMountaga <59185040+BaldeMountaga@users.noreply.github.com>
2020-09-18 08:23:35 +0000

    first commit

--- template/frontendBlog/blogin-app/src/component/HomePage.js
+++ template/frontendBlog/blogin-app/src/component/HomePage.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import  popular from  './Images/popular post1.jpg';
 import vitamin from './Images/vitamin.jpg';
-import api from'./Images/api.jpg'
+import api from'./Images/api.jpg';
+
 
 import {
     Collapse,
@@ -14,17 +15,28 @@ import {
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
 import { AiOutlineTwitter, AiFillLinkedin, AiFillEye, AiFillLike } from "react-icons/ai";
+import { FaFacebook } from "react-icons/fa";
 
 function HomePage(props) {
+
+  const [count, setCount] = useState(0);
+  
+  function increment(){
+    setCount(count + 1);
+  }
+
+  function decrement(){
+    setCount(count - 1);
+  }
     
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
 
-    return (
+  return (
+      <div>
         <div>
-          <div>
-              <Navbar  color="dark" dark expand="md" >
-                <NavbarBrand href="/">Scientist Blog</NavbarBrand>
+            <Navbar  color="dark" dark expand="md" >
+              <NavbarBrand className="brand" href="/">Scientist Blog</NavbarBrand>
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>
                   <Nav className="ml-auto" navbar>
@@ -42,51 +54,81 @@ function HomePage(props) {
             <div className="row">
               <div className="leftcolumn">
                 <div className="card">
-                  <h3>Best Evidence Yet That Vitamin D Reduces Covid-19 Risk</h3>
-                  <p>Vitamin D deficiency has been proposed as a risk factor for Covid-19 almost since the pandemic's start. 
-                    Since vitamin D supplements are cheap and widely available, and sunlight is free, it would be fabulous news 
-                    if this was true. However, despite many suggestive pieces of evidence, confirmation has been hard to find. 
-                    A new study offers probably the best evidence yet that people with vitamin D deficiency are more likely to become 
-                    infected, although it's still not as conclusive as scientists would like.
-                  </p>
-                  <div className="card-footer">
-                    <div className="card-child1"><strong>107</strong><AiFillLike size={25}/></div>
-                    <div className="card-child2"><strong>40</strong><AiFillEye size={25}/></div>
+                  <div className="card-body">
+                    <h3>Best Evidence Yet That Vitamin D Reduces Covid-19 Risk</h3>
+                    <p>Vitamin D deficiency has been proposed as a risk factor for Covid-19 almost since the pandemic's start. 
+                      Since vitamin D supplements are cheap and widely available, and sunlight is free, it would be fabulous news 
+                      if this was true. However, despite many suggestive pieces of evidence, confirmation has been hard to find.
+                    </p>
+                    <div className="card-footer">
+                      <div className="card-child1"><strong>107</strong><AiFillLike size={25}/></div>
+                      <div className="card-child2"><strong>40</strong><AiFillEye size={25}/></div>
+                    </div>
+                    <div>
+                        <h6>Comment</h6>
+                        <textarea className="comment" rows="4" cols="65" type="textarea"  placeholder="Add your comment" name="name" required />
+                        <div className="buttons">
+                          
+                          <button  type="button" className="button1">Post</button>
+                          <button  type="button" className="button2">Edit</button>
+                          <button  type="button" className="button3">Delete</button>
+                        </div>
+                    </div>
                   </div>
                 </div>
                 <div className="card">
-                  <h3>Incredible High-Res Images Of The Sun Snapped By Upgraded European Telescope</h3>
-                  <p>The year 2020 will certainly have plenty of negative memories associated with it but if we are looking for positives, 
-                    these first nine months have delivered us some incredible observations of the Sun, 
-                    from Earth and from Space. The newly upgraded GREGOR telescope has just added to these with a fantastic contribution.
-                  </p>
-                  <div className="card-footer">
-                    <div className="card-child1"><strong>107</strong><AiFillLike size={25}/></div>
-                    <div className="card-child2"><strong>90</strong><AiFillEye size={25}/></div>
+                  <div className="card-body">
+                    <h3>Incredible High-Res Images Of The Sun Snapped By Upgraded European Telescope</h3>
+                    <p>The year 2020 will certainly have plenty of negative memories associated with it but if we are looking for positives, 
+                      these first nine months have delivered us some incredible observations of the Sun, 
+                      from Earth and from Space. The newly upgraded GREGOR telescope has just added to these with a fantastic contribution.
+                    </p>
+                    <div className="card-footer">
+                      <div className="card-child1"><strong>107</strong><AiFillLike size={25}/></div>
+                      <div className="card-child2"><strong>90</strong><AiFillEye size={25}/></div>
+                    </div>
+                    <div>
+                        <h6>Comment</h6>
+                        <textarea className="comment" rows="4" cols="65" type="textarea"  placeholder="Add your comment" name="name" required />
+                        <div className="buttons">
+                          
+                          <button  type="button" className="button1">Post</button>
+                          <button  type="button" className="button2">Edit</button>
+                          <button  type="button" className="button3">Delete</button>
+                        </div>
+                    </div>
                   </div>
                 </div>
                 <div className="card">
-                  <h3>Orca That Carried Her Dead Calf For 17 Days Has Given Birth To A Healthy Baby</h3>
-                  <p>You may remember J35, the orca more affectionately known as Tahlequah, from the tragic headlines in 2018 when she carried her 
-                    stillborn calf for 17 days on a "Tour of Grief". The world’s hearts collective broke for Tahlequah.
-                  </p>
-                  <div className="card-footer">
-                    <div className="card-child1"><strong>30</strong><AiFillLike size={25}/></div>
-                    <div className="card-child2"><strong>50</strong><AiFillEye size={25}/></div>
+                  <div className="card-body">
+                    <h3>Organized Planing-Crystallizing Desire into Action</h3>
+                    <p>You have learned that everything man creates or acquires, begins in the form of DESIRE, that desire is taken
+                      on the first lap of its journey, form the abstract to the concrete, into the workshop of the IMAGINATION,
+                      where PLANS for its transition are created and organized.
+                    </p>
+                    <div className="card-footer">
+                      <div className="card-child1"><strong>30</strong><AiFillLike size={25}/></div>
+                      <div className="card-child2"><strong>50</strong><AiFillEye size={25}/></div>
+                    </div>
+                    <div>
+                        <h6>Comment</h6>
+                        <textarea className="comment" rows="4" cols="65" type="textarea"  placeholder="Add your comment" name="name" required />
+                        <div className="buttons">
+                          
+                          <button  type="button" className="button1">Post</button>
+                          <button  type="button" className="button2">Edit</button>
+                          <button  type="button" className="button3">Delete</button>
+                        </div>
+                    </div>
                   </div>
                 </div>
               </div>
               <div className="rightcolumn">
-                {/* <div className="card">
-                  <h2>Software Developer</h2>
-                  <div class="fakeimg"><img src= { Picture } alt="Profile" height='200px' width='inherit' /></div>
-                  <p>I am a motivated Software Engineer I publish about Web Devlopement in Java, C#, .NET, React, NodeJs</p>
-                </div> */}
                 <div className="card">
                   <h3>Popular Post</h3>
                   <div className="fakeimg">
                     <img src={ vitamin } alt="Research looking throug" />
-                    <p><a href="Best Evidence Yet That Vitamin D Reduces Covid-19 Risk">Best Evidence Yet That Vitamin D Reduces Covid-19 Risk</a></p>
+                    <p><a href="/">Best Evidence Yet That Vitamin D Reduces Covid-19 Risk</a></p>
                   </div><br/>
                   <div className="fakeimg"><img src={ api } alt="Research looking throug" />
                   <p><a  href="https://rapidapi.com/?utm_source=google&utm_medium=cpc&utm_campaign=smart-display&utm_content=none&gclid=EAIaIQobChMIp-zcgrvr6wIVhqh7Ch1MMgZqEAEYASAAEgKVxvD_BwE">
@@ -100,13 +142,31 @@ function HomePage(props) {
                   <div className="media">
                       <Link to="https://www.linkedin.com/feed/"><AiFillLinkedin style={{float: "right"}} size={40}/></Link>
                       <Link to="https://twitter.com/home?lang=en"><AiOutlineTwitter style={{float: "right"}} size={40}/></Link>
+                       <Link to="https://web.facebook.com/mountagha.balde.3"><FaFacebook style={{float: "right"}} size={40}/> </Link>
                   </div>
                 </div> 
               </div>
             </div>
 
             <div className="footer" >
-              @2020 All right reserved.
+              <form className="form-subscription">
+                <div class="container">
+                  <h2>Subscribe to our Newsletter</h2>
+                  <p>Lorem ipsum text about why you should subscribe to our newsletter blabla. Lorem ipsum text about why you should subscribe to our newsletter blabla.</p>
+                </div>
+
+                <div className="container">
+                  <input type="text" placeholder="Name" name="name" required />
+                  <input type="text" placeholder="Email address" name="mail" required />
+                  <label for="daily" className="chbox">
+                    <input type="checkbox" id="daily" name="subscribe" /> Daily Newsletter
+                  </label>
+                </div>
+                <div class="container">
+                  <input  type="submit" value="Subscribe" />
+                </div>
+              </form>
+              <div className="right"> @2020 All right reserved.</div>
             </div>
         </div>
     );
